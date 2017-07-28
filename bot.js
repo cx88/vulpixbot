7a6d1c66954340107d2fbbb93dadb4aa838c5595
M3rein <M3rein@gmail.com>
2017-07-28 20:38:52 +0200

    Add files via upload

--- bot.js
+++ bot.js
@@ -277,8 +277,9 @@ bot.on('message', message => {
                     console.log(id == null);
                     if (id != undefined && id != null){
                         message.channel.send("sending a message to #general...")
-                        bot.channels.get(id).send('ur late');//message.content.split('!say ' + args[0] + ' ')[1]);
-//                        bot.channels[id].send(/*message.content.split('!say ' + args[0] + ' ')[1]*/'Hello from #testing!');
+                        var chat = bot.channels.get(id)
+                        chat.sendMessage('ur late');
+//                      message.content.split('!say ' + args[0] + ' ')[1]);
                     }
                 }
             }
