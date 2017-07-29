1e310320e65c74d96a6018b75c2ac1e5ea2047a1
M3rein <M3rein@gmail.com>
2017-07-29 17:57:08 +0200

    Add files via upload

--- bot.js
+++ bot.js
@@ -402,12 +402,14 @@ bot.on('message', message => {
             else if (cmd == "quote"){
                 if (message.mentions.users.first() != undefined){
                     var user = message.mentions.users.first();
-                    var msg = message.content.split(config[id]["prefix"]+"quote " + user.username);
+                    var msg = message.content.split(`${config[id]["prefix"]}quote <@${user.id}>`);
+                    console.log(msg);
                     if (msg != undefined && msg != null && msg != "" && msg != " "){
                         if (config[id]["quotes"][user.id] == undefined){
                             config[id]["quotes"][user.id] = [
                                 msg
                             ];
+                            message.channel.send(`Quote saved!`);
                         }
                     }
                     else{
