cc010be288cda775355d2cdbe24cc416e06aa36c
M3rein <M3rein@gmail.com>
2017-07-29 17:21:27 +0200

    Add files via upload

--- bot.js
+++ bot.js
@@ -362,10 +362,10 @@ bot.on('message', message => {
             }
             else if (cmd == "rank" || cmd == "level"){
                 var user = message.member.user;
-                message.channel.send(message.mentions);
-                message.channel.send(message.mentions.length);
-                message.channel.send(message.mentions.users);
-                message.channel.send(message.mentions.users.length);
+                console.log(message.mentions);
+                console.log(message.mentions.length);
+                console.log(message.mentions.users);
+                console.log(message.mentions.users.length);
                 if (message.mentions.users.length > 0){
                     user = message.mentions.users.first();
                 }
