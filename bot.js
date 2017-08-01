fe5acd20c91ce7abde6ca0dfae8987d53e2ef664
M3rein <M3rein@gmail.com>
2017-08-01 17:30:50 +0200

    Add files via upload

--- bot.js
+++ bot.js
@@ -449,6 +449,9 @@ bot.on('message', message => {
                         options[i] = options[i].substr(0, options[i].length - 1);
                     }
                 }
+                for (i = 0; i < options.length; i++){
+                    console.log(`-${options[i]}-`);
+                }
                 if (options.length == 1){
                     message.channel.send(`Don't try to trick me!`);
                     return;
