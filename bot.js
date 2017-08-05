00d0d11820c5f0368c86b8dca6cbbc90d3397e6d
M3rein <M3rein@gmail.com>
2017-08-05 17:04:36 +0200

    Typo

--- bot.js
+++ bot.js
@@ -1684,7 +1684,7 @@ bot.on('message', message => {
                 if (config[id].roles){
                     var keys = Object.keys(config[id].roles);
                     for (i = 0; i < keys.length; i++){
-                        for (j = 0 ; j < config[id].roles[keys[i]].length; i++){
+                        for (j = 0 ; j < config[id].roles[keys[i]].length; j++){
                             active.push(`"${config[id].roles[keys[i]][j]}" on "${keys[i]}"`);
                         }
                     }
